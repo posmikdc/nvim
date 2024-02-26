c11b190e4262fd7280a947619357b111998533cf
Daniel Posmik <posmikdc@gmail.com>
2024-02-26 15:16:22 -0600

    Updated some which-key mappings for git stuff

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -6,17 +6,18 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      
-      -- Keymaps 
+
+      -- Keymaps
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
 
       -- Custom which-key descriptions
       local wk = require("which-key")
-        wk.register({
-          ["<leader>g"] = {"Git"},
-          ["<leader>gp"] = {"Preview Hunk"},
-        }) 
+      wk.register({
+        ["<leader>g"] = { "Git" },
+        ["<leader>gp"] = { "Preview Hunk" },
+        ["<leader>gt"] = { "Blame Current" },
+      })
     end
   }
 }
