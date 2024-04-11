3a2660a3ce3711c80ec5c8f020ccbad81d97f96c
Daniel Posmik <posmikdc@gmail.com>
2024-04-10 22:18:22 -0500

    Working on Vimtex to fix compiling issue with LuaLatex, not yet resolved

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,9 +4,20 @@ return {
     -- Config: https://dr563105.github.io/blog/skim-vimtex-setup/
     vim.g.tex_flavor = 'latex' -- Default tex file format
     vim.g.vimtex_view_method = 'skim' -- Choose which program to use to view PDF file
+    --vim.g.vimtex_compiler_latexmk_engines = { -- TODO: Fix compiling issue with LuaLatex! 
+      --  _ = '-lualatex',
+    --}
+    --let g:vimtex_compiler_latexmk = {
+        --\ 'options' : [
+        --\   '-shell-escape',
+        --\   '-file-line-error',
+        --\   '-synctex=1',
+        --\   '-interaction=nonstopmode',
+        --\ ],
+        --\}
     vim.g.vimtex_view_skim_sync = 1 -- Value 1 allows forward search after every successful compilation
     vim.g.vimtex_view_skim_activate = 1 -- Value 1 allows change focus to skim after command `:VimtexView` is given
-   
+    
     -- Custom which-key descriptions
     local wk = require("which-key")
       wk.register({
