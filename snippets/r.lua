1ef12522a3303e4136d2d0908eb18bd1059762f0
Daniel Posmik <posmikdc@gmail.com>
2024-05-24 11:12:51 +0200

    Pre-reorganization into dotfiles

--- snippets/r.lua
+++ snippets/r.lua
@@ -26,11 +26,19 @@ local conds_expand = require("luasnip.extras.conditions.expand")
 local date = function()
   return { os.date('%Y-%m-%d') }
 end
+
+-- Function to create aligned subsection header
+local function aligned_subsection(args)
+  local title = args[1][1] or ""
+  local total_length = 80 -- Adjust this value as necessary for your alignment needs
+  local filler_length = total_length - #title - 2
+  return string.rep("=", filler_length)
+end
 -- }}}
 
 -- Snippets {{{
 ls.add_snippets("r", {
-  -- R Script Template
+  -- R Script Header
   s("meta", {
     t({
       "################################################################################",
@@ -57,10 +65,28 @@ ls.add_snippets("r", {
       "### " }),
     i(3, "section_title"),
     t({ "",
-      "################################################################################", 
+      "################################################################################",
       "" }),
+    i(0)
+  }),
+  -- R Script Section Header
+  s("section", {
+    t({
+      "################################################################################",
+      "### " }),
+    i(1, "section_title"),
+    t({ "",
+      "################################################################################",
+      "" }),
+    i(0)
+  }),
+  -- R Script Subsection Header
+  s("subsection", {
+    t("# "),
+    i(1, "subsection_title"),
+    f(aligned_subsection, { 1 }),
+    t({ "", "" }),
     i(0)
   }),
 })
 -- }}}
-
