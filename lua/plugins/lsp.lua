a83ee33dbba20d08146ac7ec57d763c3ad33e1cd
Daniel Posmik <posmikdc@gmail.com>
2025-01-24 15:27:34 -0500

    finally fixed slime and otter for quarto

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,5 @@
 return {
-  {
-    -- for lsp features in code cells / embedded code
-    'jmbuhr/otter.nvim',
-    dev = false,
-    dependencies = {
-      {
-        'neovim/nvim-lspconfig',
-        'nvim-treesitter/nvim-treesitter',
-      },
-    },
-    opts = {
-      verbose = {
-        no_code_found = false,
-      },
-      buffers = {
-        -- if set to true, the filetype of the otterbuffers will be set.
-        -- otherwise only the autocommand of lspconfig that attaches
-        -- the language server will be executed without setting the filetype
-        set_filetype = true,
-      },
-    },
-    config = function()
-      -- Activate Otter for Quarto files
-      vim.api.nvim_create_autocmd({ "BufEnter" }, {
-        pattern = { "*.qmd", "*.rmd", "*.md" },
-        callback = function()
-          require('otter').activate()
-        end,
-      })
-    end,
-  },
+
   {
     "williamboman/mason.nvim",
     lazy = false,
@@ -37,6 +7,7 @@ return {
       require("mason").setup()
     end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
@@ -44,31 +15,65 @@ return {
       auto_install = true,
     },
   },
+
+  {
+    "jmbuhr/otter.nvim",
+    dev = false,
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+    },
+    opts = {
+      verbose = {
+        no_code_found = false,
+      },
+    },
+  },
+
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      
+
       lspconfig.tsserver.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
-      
+
       lspconfig.html.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
-      
+
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
-      
+
       lspconfig.r_language_server.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        settings = {
+          r = {
+            lsp = {
+              rich_documentation = false,
+            },
+          },
+        },
       })
-      
+
       lspconfig.pyright.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+            },
+          },
+        },
       })
 
       -- Custom which-key descriptions
