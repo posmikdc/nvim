91a41cb2cc5b27b410404e03b0f8e0a7c2377472
Daniel Posmik <posmikdc@gmail.com>
2025-02-03 16:19:42 -0500

    Fixed otter by manual keybinding via LspInfo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,162 +1,119 @@
 return {
-  {
-    "jmbuhr/otter.nvim",
-    dev = false,
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        "nvim-treesitter/nvim-treesitter",
-      },
-    },
-    config = function()
-      -- Configure Otter
-      local otter = require("otter")
-      otter.setup({
-        lsp = {
-          diagnostic_update_events = { "BufWritePost" },
-          root_dir = function(_, bufnr)
-            return vim.fs.root(bufnr or 0, {
-              ".git",
-              "_quarto.yml",
-              "package.json",
-            }) or vim.fn.getcwd(0)
-          end,
-        },
-        buffers = {
-          set_filetype = false,
-          write_to_disk = false,
-        },
-        strip_wrapping_quote_characters = { "'", '"', "`" },
-        handle_leading_whitespace = true,
-        extensions = {},
-        debug = false,
-        verbose = {
-          no_code_found = false,
-        },
-      })
+  
+  -- Code chunk LSP support for .qmd
+	{
+		"jmbuhr/otter.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("otter").setup({
+				lsp = {
+					diagnostic_update_events = { "BufWritePost" },
+					rootdir = function(_, bufnr)
+						return vim.fs.root(bufnr or 0, {
+							".git",
+							"_quarto.yml",
+							"package.json",
+						}) or vim.fn.getcwd(0)
+					end,
+				},
+				buffers = {
+					set_filetype = true,
+					write_to_disk = false,
+				},
+			})
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+				group = vim.api.nvim_create_augroup("OtterActivation", { clear = true }),
+				pattern = "*.qmd",
+				callback = function()
+					require("otter").activate()
+				end,
+			})
+		end,
+	},
 
-      -- Create an autocmd group for Otter
-      local otter_group = vim.api.nvim_create_augroup("OtterActivation", { clear = true })
+  -- Mason LSP Installer 
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-      -- Create autocmd for .qmd files
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-        group = otter_group,
-        pattern = "*.qmd",
-        callback = function()
-          -- Activate Otter with default settings for all languages
-          otter.activate(nil, true, true, nil)
-          print("Otter activated for Quarto document")
-        end,
-      })
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.r_language_server.setup({
-        capabilities = capabilities,
-        settings = {
-          r = {
-            lsp = {
-              rich_documentation = false,
-            },
-          },
-        },
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              diagnosticMode = "workspace",
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.r_language_server.setup({
-        capabilities = capabilities,
-        settings = {
-          r = {
-            lsp = {
-              rich_documentation = false,
-            },
-          },
-        },
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              diagnosticMode = "workspace",
-            },
-          },
-        },
-      })
-      lspconfig.julials.setup({
-        capabilities = capabilities,
-      })
-    end,
-  },
+  -- Mason LSP Config 
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+
+  -- My LSP Configurations
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			local servers = {
+				ts_ls = {},
+				html = {},
+				lua_ls = {},
+				r_language_server = {
+					capabilities = capabilities,
+					filetypes = { "r", "rmd" },
+				},
+				pyright = {
+					settings = {
+						python = {
+							analysis = {
+								autoSearchPaths = true,
+								useLibraryCodeForTypes = true,
+							},
+						},
+					},
+				},
+				julials = {},
+			}
+			for server, config in pairs(servers) do
+				config.capabilities = capabilities
+				lspconfig[server].setup(config)
+			end
+
+			-- Add keymapping to force LSP initialization
+			vim.keymap.set("n", "<leader>l", function()
+				-- Get active LSP clients
+				local active_clients = vim.lsp.get_active_clients()
+
+				-- Start all configured servers if they're not already running
+				for server_name, _ in pairs(servers) do
+					local found = false
+					for _, client in ipairs(active_clients) do
+						if client.name == server_name then
+							found = true
+							break
+						end
+					end
+
+					if not found then
+						-- Start the server using lspconfig
+						lspconfig[server_name].setup({})
+					end
+				end
+
+				-- Force Otter activation if it's a qmd file
+				local ft = vim.bo.filetype
+				if ft == "quarto" or ft == "qmd" then
+					require("otter").activate()
+				end
+
+				-- Show LSP info
+				vim.cmd("LspInfo")
+			end, { desc = "LSP Initialization" })
+		end,
+	},
 }
